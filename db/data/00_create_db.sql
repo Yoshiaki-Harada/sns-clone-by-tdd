e126459fa72d64dd40a53f2b276b70d259c11a00
haradayoshiaki <y.team.kobe@gmail.com>
2020-06-06 17:45:37 +0900

    dbを作成

---
+++ db/data/00_create_db.sql
@@ -0,0 +1,77 @@
+CREATE database sns_db WITH
+    OWNER developer
+    TEMPLATE  template0
+ENCODING 'UTF-8'
+LC_COLLATE 'ja_JP.utf8'
+LC_CTYPE 'ja_JP.utf8'
+TABLESPACE default
+connection limit -1;
+
+\connect sns_db
+
+create table users
+(
+    id         uuid                                not null
+        constraint users_pkey
+            primary key,
+    mail       varchar(100)                        not null,
+    name       varchar(100)                        not null,
+    birthday    date                                not null,
+    created_at timestamp default CURRENT_TIMESTAMP not null,
+    updated_at timestamp default CURRENT_TIMESTAMP not null
+);
+
+create table messages
+(
+    id         uuid                                not null
+        constraint messages_pkey
+            primary key,
+    user_id    uuid                                not null
+        constraint messages_user_id_fkey
+            references users
+            on delete cascade,
+    text       varchar(500),
+    created_at timestamp default CURRENT_TIMESTAMP not null,
+    updated_at timestamp default CURRENT_TIMESTAMP not null
+);
+
+create table comments
+(
+    id         uuid                                not null
+        constraint comments_pkey
+            primary key,
+    user_id    uuid                                not null
+        constraint comments_user_id_fkey
+            references users
+            on delete cascade,
+    message_id uuid                                not null
+        constraint comments_message_id_fkey
+            references messages
+            on delete cascade,
+    text       varchar(500)                        not null,
+    created_at timestamp default CURRENT_TIMESTAMP not null,
+    updated_at timestamp default CURRENT_TIMESTAMP not null
+);
+
+create table tags
+(
+    id   uuid         not null
+        constraint tags_pkey
+            primary key,
+    name varchar(100) not null
+);
+
+create table tag_map
+(
+    id         uuid not null
+        constraint tag_map_pkey
+            primary key,
+    tag_id     uuid not null
+        constraint tag_map_tag_id_fkey
+            references tags
+            on delete cascade,
+    message_id uuid not null
+        constraint tag_map_message_id_fkey
+            references messages
+            on delete cascade
+);
---
+++ db/run.sh
@@ -0,0 +1,1 @@
+docker run -d --rm --name sns-db -e POSTGRES_PASSWORD=developer -e POSTGRES_USER=developer -p 5432:5432 sns-db
