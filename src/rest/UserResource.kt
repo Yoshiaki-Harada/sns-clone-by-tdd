2923c2b3a352720bb098978bfbc31920d242acb1
haradayoshiaki <y.team.kobe@gmail.com>
2020-06-07 14:36:27 +0900

    errorハンドリングを行った

--- src/rest/UserResource.kt
+++ src/rest/UserResource.kt
@@ -1,5 +1,6 @@
 package com.harada.rest
 
+import com.google.gson.JsonSyntaxException
 import com.harada.Injector
 import com.harada.domain.model.user.Mail
 import com.harada.domain.model.user.User
@@ -7,6 +8,9 @@ import com.harada.domain.model.user.UserName
 import com.harada.usecase.IUserCreateUseCase
 import io.ktor.application.Application
 import io.ktor.application.call
+import io.ktor.application.install
+import io.ktor.features.StatusPages
+import io.ktor.http.HttpStatusCode
 import io.ktor.request.receive
 import io.ktor.response.respond
 import io.ktor.routing.post
@@ -14,6 +18,7 @@ import io.ktor.routing.routing
 import mu.KotlinLogging
 import org.kodein.di.Kodein
 import org.kodein.di.generic.instance
+import java.text.ParseException
 import java.text.SimpleDateFormat
 
 
@@ -22,18 +27,33 @@ fun Application.userModule() {
 }
 
 fun Application.userModuleWithDepth(kodein: Kodein) {
-    val logger = KotlinLogging.logger {}
+    install(StatusPages) {
+        exception<ParseException> { cause ->
+            val errorMessage = cause.message ?: "Unknown error"
+            call.respond(HttpStatusCode.BadRequest, ErrorResponse(errorMessage))
+        }
+        exception<JsonSyntaxException> { cause ->
+            val errorMessage = cause.message ?: "Unknown error"
+            call.respond(HttpStatusCode.BadRequest, ErrorResponse(errorMessage))
+        }
+        exception<IllegalArgumentException> { cause ->
+            val errorMessage = cause.message ?: "Unknown error"
+            call.respond(HttpStatusCode.BadRequest, ErrorResponse(errorMessage))
+        }
+    }
     val createUseCase by kodein.instance<IUserCreateUseCase>()
-    val sdFormat = SimpleDateFormat("yyyy-MM-dd")
-
     routing {
         post("/users") {
             val json = call.receive<RequestUser>()
-            val userId = createUseCase.execute(User(UserName(json.name), Mail(json.mail), sdFormat.parse(json.birthday)))
+            val userId =
+                createUseCase.execute(User(UserName(json.name), Mail(json.mail), parseDate(json.birthday)))
             call.respond(ResponseUserId(userId.value.toString()))
         }
     }
 }
 
+data class ErrorResponse(val errorMessage: String)
+
+fun parseDate(date: String) = runCatching { SimpleDateFormat("yyyy-MM-dd").parse(date) }.getOrThrow()
 data class RequestUser(val name: String, val mail: String, val birthday: String)
 data class ResponseUserId(val userId: String)
