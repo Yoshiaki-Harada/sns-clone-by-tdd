c08ee23f044430dab57c8b079acadaf36a59b7e3
haradayoshiaki <y.team.kobe@gmail.com>
2020-06-07 16:52:36 +0900

    テストのリファクタリング

--- test/rest/UserResourceKtTest.kt
+++ test/rest/UserResourceKtTest.kt
@@ -20,130 +20,98 @@ import io.ktor.server.testing.TestApplicationCall
 import io.ktor.server.testing.handleRequest
 import io.ktor.server.testing.setBody
 import io.ktor.server.testing.withTestApplication
-import io.mockk.clearMocks
 import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
-import org.junit.Test
 import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
 import org.kodein.di.Kodein
 import org.kodein.di.generic.bind
 import org.kodein.di.generic.singleton
 import java.util.*
 
-internal class UserResourceKtTest {
-    private val gson = GsonBuilder().setPrettyPrinting().create()
+class UserResourceKtTest {
 
-    val createUseCase = mockk<IUserCreateUseCase>()
+    @Nested
+    inner class CreateUser {
+        val gson = GsonBuilder().setPrettyPrinting().create()
 
-    @BeforeAll
-    fun init() {
-        clearMocks(createUseCase)
-    }
-
-    @Test
-    fun `userを登録することができる`() {
-        every {
-            createUseCase.execute(any<User>())
-        } returns UserId(UUID.fromString("A0EEBC99-9C0B-4EF8-BB6D-6BB9BD380A11"))
+        val createUseCase = mockk<IUserCreateUseCase>() {
+            every { this@mockk.execute(any()) } returns UserId(UUID.fromString("A0EEBC99-9C0B-4EF8-BB6D-6BB9BD380A11"))
+        }
 
         val testKodein = Kodein {
             bind<IUserCreateUseCase>() with singleton { createUseCase }
         }
-        invokeTestApplication(
-            moduleFunction = {
-                userModuleWithDepth(testKodein).apply {
-                    install(ContentNegotiation) { gson { setPrettyPrinting() } }
+
+        @Test
+        fun `userを登録することができる`() {
+            invokeWithTestUserCreateApplication(
+                testKodein = testKodein,
+                body = gson.toJson(RequestUser("Tanaka Taro", "test@gmail.com", "1990-01-01")),
+                assert = {
+                    verify { createUseCase.execute(createUser()) }
+                    assertEquals(HttpStatusCode.OK, response.status())
                 }
-            },
-            path = "/users",
-            method = HttpMethod.Post,
-            body = gson.toJson(RequestUser("Tanaka Taro", "test@gmail.com", "1990-01-01")),
-            contentType = ContentType.Application.Json,
-            assert = {
-                verify { createUseCase.execute(createUser()) }
-                assertEquals(HttpStatusCode.OK, response.status())
-            }
-        )
-    }
-
-
-    @Test
-    fun `不正な日付フォーマットの誕生日のUserを登録するとStatus Code 400が返却される`() {
-        every {
-            createUseCase.execute(any<User>())
-        } returns UserId(UUID.fromString("A0EEBC99-9C0B-4EF8-BB6D-6BB9BD380A11"))
-
-        val testKodein = Kodein {
-            bind<IUserCreateUseCase>() with singleton { createUseCase }
+            )
         }
-        invokeTestApplication(
-            moduleFunction = {
-                userModuleWithDepth(testKodein).apply {
-                    install(ContentNegotiation) { gson { setPrettyPrinting() } }
-                }
-            },
-            path = "/users",
-            method = HttpMethod.Post,
-            body = gson.toJson(createRequestUser(birthday = "1990-")),
-            contentType = ContentType.Application.Json,
-            assert = {
-                assertEquals(HttpStatusCode.BadRequest, response.status())
-            }
-        )
-    }
-    @Test
-    fun `不正なフォーマットのUserを登録するとStatus Code 400が返却される`() {
-        every {
-            createUseCase.execute(any<User>())
-        } returns UserId(UUID.fromString("A0EEBC99-9C0B-4EF8-BB6D-6BB9BD380A11"))
 
-        val testKodein = Kodein {
-            bind<IUserCreateUseCase>() with singleton { createUseCase }
-        }
-        invokeTestApplication(
-            moduleFunction = {
-                userModuleWithDepth(testKodein).apply {
-                    install(ContentNegotiation) { gson { setPrettyPrinting() } }
+        @Test
+        fun `不正な日付フォーマットの誕生日のUserを登録するとStatus Code 400が返却される`() {
+            invokeWithTestUserCreateApplication(
+                testKodein = testKodein,
+                body = gson.toJson(createRequestUser(birthday = "1990-")),
+                assert = {
+                    assertEquals(HttpStatusCode.BadRequest, response.status())
                 }
-            },
-            path = "/users",
-            method = HttpMethod.Post,
-            body = "{\"nam\" : \"Taro\"",
-            contentType = ContentType.Application.Json,
-            assert = {
-                assertEquals(HttpStatusCode.BadRequest, response.status())
-            }
-        )
-    }
-    @Test
-    fun `フィールドが揃っていないUserを登録するとStatus Code 400が返却される`() {
-        every {
-            createUseCase.execute(any<User>())
-        } returns UserId(UUID.fromString("A0EEBC99-9C0B-4EF8-BB6D-6BB9BD380A11"))
+            )
+        }
 
-        val testKodein = Kodein {
-            bind<IUserCreateUseCase>() with singleton { createUseCase }
-        }
-        invokeTestApplication(
-            moduleFunction = {
-                userModuleWithDepth(testKodein).apply {
-                    install(ContentNegotiation) { gson { setPrettyPrinting() } }
+        @Test
+        fun `不正なフォーマットのUserを登録するとStatus Code 400が返却される`() {
+            invokeWithTestUserCreateApplication(
+                testKodein = testKodein,
+                body = "{\"nam\" : \"Taro\"",
+                assert = {
+                    assertEquals(HttpStatusCode.BadRequest, response.status())
                 }
-            },
-            path = "/users",
-            method = HttpMethod.Post,
-            body = "{\"name\" : \"Taro\"}",
-            contentType = ContentType.Application.Json,
-            assert = {
-                assertEquals(HttpStatusCode.BadRequest, response.status())
-            }
-        )
+            )
+        }
+
+        @Test
+        fun `フィールドが揃っていないUserを登録するとStatus Code 400が返却される`() {
+            invokeWithTestUserCreateApplication(
+                testKodein = testKodein,
+                body = "{\"name\" : \"Taro\"}",
+                assert = {
+                    assertEquals(HttpStatusCode.BadRequest, response.status())
+                }
+            )
+        }
+
+        fun invokeWithTestUserCreateApplication(
+            testKodein: Kodein,
+            body: String?,
+            assert: TestApplicationCall.() -> Unit
+        ) {
+            invokeWithTestApplication(
+                moduleFunction = {
+                    userModuleWithDepth(testKodein).apply {
+                        install(ContentNegotiation) { gson { setPrettyPrinting() } }
+                    }
+                },
+                path = "/users",
+                method = HttpMethod.Post,
+                body = body,
+                contentType = ContentType.Application.Json,
+                assert = assert
+            )
+        }
     }
 }
 
-fun invokeTestApplication(
+fun invokeWithTestApplication(
     moduleFunction: Application.() -> Unit,
     path: String,
     method: HttpMethod,
